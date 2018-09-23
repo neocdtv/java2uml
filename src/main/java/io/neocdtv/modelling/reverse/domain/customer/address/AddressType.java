c44737ced64b14be648660ad217adebcfa68e80c
Krzysztof Wolf <krzysztof.wolf@nttdata.com>
2018-09-23 16:22:58 +0200

    udpate gitignore

---
+++ src/main/java/io/neocdtv/modelling/reverse/domain/customer/address/AddressType.java
@@ -0,0 +1,10 @@
+package io.neocdtv.modelling.reverse.domain.customer.address;
+
+/**
+ * AddressType
+ *
+ * @author Krzysztof Wolf (WOLFKR)
+ * @since 11.09.2018.
+ */
+public enum AddressType {
+}
