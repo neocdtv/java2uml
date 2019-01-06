7f208c8d1ee1e03d6b53db8eec12a65258b47b54
Krzysztof Wolf <krzysztof.wolf@partner.bmw.de>
2019-01-06 16:49:20 +0100

    add comments

--- generate-win.sh
+++ generate-win.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# using git-bash on windows and the directory notation: /c/Projects/java2uml causes somewhere a nullpointer exception, so using the windows notation with \\,
+# which by the way also works in cygwin terminal
 java -jar target/java2uml.jar -packageInputConfigs="C:\\Projects\\java2uml\\packageInputConfigs-win.json" -outputDir="C:\\Projects\\java2uml"
